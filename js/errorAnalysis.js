7d3592464b7801998ff890c40fa8f2e5d2b75b27
stsourlidakis <s.tsourlidakis@hotmail.com>
2014-11-21 01:05:45 +0200

    experimental values table update, bug fixes

--- js/errorAnalysis.js
+++ js/errorAnalysis.js
@@ -1,4 +1,5 @@
 var active;	//id of active window
+var experimentalValuesDone=false;	//id of active window
 var n;//# of error buttons/tables
 var tableName = ["Error in a measurement", "Error in an average", "Standard Deviation", "sources of error"];
 var logO = {
@@ -39,7 +40,8 @@ $( document ).ready(function() {
 		reset_table($(this).closest('.ErrorContainer').attr('id'));
 	});
 	//Hide all error tables,show the first one and set it as the active table
-	show_table(5);	//Must be placed after event handlers so table 0 can be logged as a member of "ErrorButtons" class before show_table() change its class to "ErrorButtonsActive" 
+	show_table(5);	//Must be placed after event handlers so table 0 can be logged as a member of "ErrorButtons" class before show_table() change its class to "ErrorButtonsActive"	
+	disable_error_buttons();
 	$("html").keypress(function(e){
 	  if(e.keyCode==9) {	//tab
 			test();
@@ -129,8 +131,9 @@ function test(){
 function show_table(id)
 {
 	if(active==id)return;	//don't try to show an already active error table
+	if( !experimentalValuesDone && (id==0||id==1||id==2) )return;
 	for (var i=0;i<n;i++){	//hide all error tables and set their button on the inactive class
-		if(id!=i)hide_table(i);	
+		if(id!=i)hide_table(i);
 	}
 	hide_info();
 	document.getElementById(id).style.display = "block";
@@ -150,9 +153,21 @@ function hide_info()
 function hide_table(id)
 {
 	document.getElementById(id).style.display = "none";
-	document.getElementById("errorButton"+id).className = "ErrorButtons";
+	if( experimentalValuesDone || (id!=0&&id!=1&&id!=2) )document.getElementById("errorButton"+id).className = "ErrorButtons";
 	
 	if(id==active)active=-1;
+}
+function disable_error_buttons()
+{
+	document.getElementById("errorButton0").className = "ErrorButtonsInactive";
+	document.getElementById("errorButton1").className = "ErrorButtonsInactive";
+	document.getElementById("errorButton2").className = "ErrorButtonsInactive";
+}
+function enable_error_buttons()
+{
+	document.getElementById("errorButton0").className = "ErrorButtons";
+	document.getElementById("errorButton1").className = "ErrorButtons";
+	document.getElementById("errorButton2").className = "ErrorButtons";
 }
 function toggle_info(id)
 {
@@ -193,6 +208,10 @@ function reset_table(id)
 	$('#'+id+' .'+id+'-questions').hide();
 	$('#'+id+' input:checkbox').removeAttr('checked');
 	//$('#'+id+' input:radio').attr("checked", true);
+	if(id==4){
+		$('#4-screen-0').closest('.row').hide();	//hide "corrected set of measurement" screen.
+		$('#4-screen-1').closest('.row').find('.p1').html('The mean value is:');
+	}
 }
 function calculateMeasurementError()
 {
@@ -238,15 +257,14 @@ function calculateAverageError(){
 }
 
 function calculateStandardDeviation()
-{	
-	var i, maxDevFromMean, _temp;
+{
+	var i;
 	var table = 2;	//table id
-	var expValTable = 0;	//Table that contains the experimental value input field to be auto-filled.
 	if( !testInput(table) ){
-		alert("Invalid input\nOnly numerical values are allowed.\nFor decimal numbers please use \".\"\nIn order to separate values use \",\"\nMore than one values should be defined.");
+		alert("Invalid input\nOnly numerical values are allowed.\nFor decimal numbers please use \".\"\nIn order to separate values use space\nMore than one values should be defined.");
 		return;
 	}
-	var values = document.getElementById(table+'-0').value.split(",");
+	var values = document.getElementById(table+'-0').value.split(" ");
 	var avg=0;
 	var avg1=0;
 	for (i = 0; i < values.length; i++){	//Average calculation
@@ -259,17 +277,8 @@ function calculateStandardDeviation()
 	}
 	avg1/=(values.length-1);
 	avg = avg.toFixed(3);
-	maxDevFromMean = Math.abs(avg-values[0]);	//Max probable error calculation
-	for (i = 1; i < values.length; i++){
-		_temp = Math.abs(avg-values[i]);
-		if( _temp > maxDevFromMean){
-			maxDevFromMean = _temp;
-		}
-	}
 	document.getElementById(table+'-screen'+'-0').innerHTML=avg;
-	document.getElementById(expValTable+'-1').value=avg;
 	document.getElementById(table+'-screen'+'-1').innerHTML=Math.sqrt(avg1).toFixed(3);
-	document.getElementById(table+'-screen'+'-2').innerHTML= maxDevFromMean.toFixed(3);	
 	
 	var questions = document.getElementsByClassName(table+"-questions");
 	for (var i=0;i<questions.length;i++)
@@ -277,39 +286,73 @@ function calculateStandardDeviation()
 }
 
 function calculateMeanValue(){
-	var i, avg, table=4;
+	var i, maxProbableError, avg, newValues, table=4;
 	var expValTable = 0;	//Table that contains the experimental value input field to be auto-filled.
 	var sdTable = 2;	//standard deviation table
+	var systematicError = $('#4-1').val().trim();	//trim removes whitespace characters 
+	if( systematicError=='' || isNaN(systematicError) ){
+		$('#4-1').val(0);
+		systematicError=0;
+	}
 	if( !testInput(table) ){
-		alert("Invalid input\nOnly numerical values are allowed.\nFor decimal numbers please use \".\"\nIn order to separate values use \",\"\nMore than one values should be defined.");
+		alert("Invalid input\nOnly numerical values are allowed.\nFor decimal numbers please use \".\"\nIn order to separate values use space\nMore than one values should be defined.");
 		return;
 	}
-	var values = document.getElementById(table+'-0').value.split(",");
-	var avg=0;
+	$('#'+table+'-screen'+'-0').closest('.row').hide();
+	var values = document.getElementById(table+'-0').value.split(" ");
+	avg=0;
+	newValues='';
 	for (i = 0; i < values.length; i++){	//Average calculation
 		values[i]=parseFloat(values[i]);
-		avg+=values[i];
+		avg+=values[i]-systematicError;
+		newValues +=(i==values.length-1)?(values[i]-systematicError).toFixed(3).replace(/\.000$/, ''):(values[i]-systematicError).toFixed(3).replace(/\.000$/, '')+' ';	//ifthenelse in order to prevent a stray space after the last value. Regex to remove no needed zeroes
 		}
+	maxProbableError = Math.abs( (Math.min.apply(Math, values)-Math.max.apply(Math, values)) / 2);	//Max probable error calculation
 	avg = (avg/values.length).toFixed(3);
-	document.getElementById(table+'-screen'+'-0').innerHTML=avg;
-	document.getElementById(sdTable+'-0').value = document.getElementById(table+'-0').value
+	document.getElementById(table+'-screen'+'-1').innerHTML=avg;
 	document.getElementById(expValTable+'-1').value=avg;
+	document.getElementById(table+'-screen'+'-2').innerHTML= maxProbableError.toFixed(3);
+	
+	if(systematicError==0){
+		document.getElementById(sdTable+'-0').value = document.getElementById(table+'-0').value;
+		$('#'+table+'-screen-1').closest('.row').find('.p1').html('You have now calculated the mean value of your measurements\' set. This mean value is your experimental value.');
+	}
+	else{
+		document.getElementById(sdTable+'-0').value = newValues;
+		document.getElementById(table+'-screen'+'-0').value=newValues;
+		$('#'+table+'-screen'+'-0').closest('.row').show();
+		$('#'+table+'-screen-1').closest('.row').find('.p1').html('The mean value of the corrected measurements is:');
+	}
+	experimentalValuesDone = true;
+	enable_error_buttons();
 }
 
 function testInput(table){	//returns true for numerical values and false for everything else
-	if( (table!=2) && (table!=4) )		//table 2 has only 1 form to check
+
+	if(table==2)		//table 2 has only 1 form to check
+	{
+		var values = document.getElementById(table+'-0').value.split(" ");
+		if(values.length<2)return false;
+		for (var i = 0; i < values.length; i++){
+			if(isNaN(values[i]) || values[i]=="" || values[i]==" ")return false;
+		}
+	}
+	else if(table==4)
+	{
+		var values = document.getElementById(table+'-0').value.split(" ");
+		if(values.length<2)return false;
+		for (var i = 0; i < values.length; i++){
+			if(isNaN(values[i]) || values[i]=="" || values[i]==" ")return false;
+		}
+		inputValue=document.getElementById(table+'-1').value;
+		if(isNaN(inputValue) || inputValue=="" || ( ( inputValue.indexOf(' ')==0 && inputValue.length-1==0 ) || ( inputValue.indexOf(' ')==inputValue.length-1 && inputValue.length-1==0 ) ) )return false;
+	}	
+	else
 	{
 		inputValue=document.getElementById(table+'-0').value;
 		if(isNaN(inputValue) || inputValue=="" || ( ( inputValue.indexOf(' ')==0 && inputValue.length-1==0 ) || ( inputValue.indexOf(' ')==inputValue.length-1 && inputValue.length-1==0 ) ) )return false;	//Third argument checks for a space but allows one at the start or the end of the number
 		inputValue=document.getElementById(table+'-1').value;
 		if(isNaN(inputValue) || inputValue=="" || ( ( inputValue.indexOf(' ')==0 && inputValue.length-1==0 ) || ( inputValue.indexOf(' ')==inputValue.length-1 && inputValue.length-1==0 ) ) )return false;
-	}
-	else
-	{
-		var values = document.getElementById(table+'-0').value.split(",");
-		for (var i = 0; i < values.length; i++){
-			if(isNaN(values[i]) || values[i]=="" || values[i]==" " || values.length<2)return false;
-		}
 	}
 	return true;
 }
